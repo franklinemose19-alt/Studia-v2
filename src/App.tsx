941a70f39291a01aae6127beed269791cce5dc49
franklinemose19-alt <franklinemose19@gmail.com>
2026-06-06 21:44:44 +0300

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -12,6 +12,7 @@ export default function App() {
         <Route path="/login" element={<Login />} />
         <Route path="/signup" element={<Signup />} />
         <Route path="/dashboard" element={<Dashboard />} />
+        <Route path="/recording" element={<Recording />} />
       </Routes>
     </BrowserRouter>
   )
