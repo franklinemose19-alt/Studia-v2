baf92c2e81cb54a4f4bc35e6b88a5a41d5bafeb2
franklinemose19-alt <franklinemose19@gmail.com>
2026-06-13 17:52:59 +0300

    Update get-supabase-config.js

--- api/get-supabase-config.js
+++ api/get-supabase-config.js
@@ -1,6 +1,11 @@
 export default function handler(req, res) {
+  let url = process.env.SUPABASE_URL || ''
+  
+  // Remove /rest/v1/ if it's there
+  url = url.replace('/rest/v1/', '')
+  
   return res.status(200).json({
-    url: process.env.SUPABASE_URL,
+    url: url,
     key: process.env.SUPABASE_ANON_KEY,
   })
 }
