d772e914f83495cfa09363a1e4540d36ea814d08
franklinemose19-alt <franklinemose19@gmail.com>
2026-06-16 09:07:06 +0300

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import AITools from './pages/AITools'
+import AITools from './page/AITools'
 import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom'
 import ProtectedRoute from './lib/ProtectedRoute'
 
